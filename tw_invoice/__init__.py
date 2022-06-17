4f38f4a77d157051900878b63b7761ac32b7024c
Tomy Hsieh <tomy0000000@gmail.com>
2022-06-18 01:16:40 +0800

    ⌛️ ci: Generate version in CI

--- tw_invoice/__init__.py
+++ tw_invoice/__init__.py
@@ -1,15 +1,2 @@
 """🇹🇼🧾 Python SDK for accessing Taiwan E-Inovice API"""
-import os
-from datetime import datetime
-from random import randint
-
 from .app_client import AppAPIClient  # noqa
-
-VERSION = datetime.today().strftime("%Y.%-m.%d") + "b"  # b represent beta version
-
-GITHUB_ACTION = os.getenv("GITHUB_ACTION")
-
-if GITHUB_ACTION == "publish-to-test-pypi":
-    VERSION = f"{VERSION}{randint(0, 10000)}"
-
-__version__ = VERSION
