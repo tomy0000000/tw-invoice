d4613cf46dfafa3119074bf34777b450b61abae2
Tomy Hsieh <tomy0000000@gmail.com>
2022-06-17 15:36:27 +0800

    🛠 fix: version
    conforming PEP 440

--- tw_invoice/__init__.py
+++ tw_invoice/__init__.py
@@ -1,15 +1,15 @@
 """🇹🇼🧾 Python SDK for accessing Taiwan E-Inovice API"""
 import os
 from datetime import datetime
-from uuid import uuid4
+from random import randint
 
 from .app_client import AppAPIClient  # noqa
 
-VERSION = datetime.today().strftime("%Y.%m.%d") + "b"  # b represent beta version
+VERSION = datetime.today().strftime("%Y.%-m.%d") + "b"  # b represent beta version
 
 GITHUB_ACTION = os.getenv("GITHUB_ACTION")
-GITHUB_SHA = os.getenv("GITHUB_SHA")
+
 if GITHUB_ACTION == "publish-to-test-pypi":
-    VERSION = f"{VERSION}-{GITHUB_SHA}-{str(uuid4())[:8]}"
+    VERSION = f"{VERSION}{randint(0, 10000)}"
 
 __version__ = VERSION
