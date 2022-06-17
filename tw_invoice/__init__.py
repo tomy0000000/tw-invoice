ae7a8edcabb37bf37cc357187110b13731cb229b
Tomy Hsieh <tomy0000000@gmail.com>
2022-06-17 15:04:52 +0800

    🛠 fix: Fix wrong import name bug
    Also implement a dynamic version generator when publishing test package

--- tw_invoice/__init__.py
+++ tw_invoice/__init__.py
@@ -1,6 +1,15 @@
 """🇹🇼🧾 Python SDK for accessing Taiwan E-Inovice API"""
+import os
 from datetime import datetime
+from uuid import uuid4
 
-from .app_client import AppClient  # noqa
+from .app_client import AppAPIClient  # noqa
 
-__version__ = datetime.today().strftime("%Y.%m.%d") + "b"  # b represent beta version
+VERSION = datetime.today().strftime("%Y.%m.%d") + "b"  # b represent beta version
+
+GITHUB_ACTION = os.getenv("GITHUB_ACTION")
+GITHUB_SHA = os.getenv("GITHUB_SHA")
+if GITHUB_ACTION == "publish-to-test-pypi":
+    VERSION = f"{VERSION}-{GITHUB_SHA}-{str(uuid4())[:8]}"
+
+__version__ = VERSION
